63a292b174e4df37b7c43809f271e0814f4f0e23
Anna Myslyva <anna.myslyva@kpark.de>
2023-03-04 18:03:11 +0100

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="text-left">Weather App</h1>
+        <button className="btn btn-outline-primary">Hello</button>
+      </div>
+    </div>
+  );
 }
